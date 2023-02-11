973b747fe17ab43833b72a2b41aaade36e772750
Yaroslav Oliinyk <4106677@gmail.com>
2023-02-11 10:53:33 +0200

    start

---
+++ function.js
@@ -0,0 +1,6 @@
+const sum = (num1, num2) => num1 + num2;
+const minus = (num1, num2) => num1 - num2;
+const multipl = (num1, num2) => num1 * num2;
+const divide = (num1, num2) => num1 / num2;
+
+module.exports = {sum,minus,multipl,divide};
